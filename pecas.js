04718623e9ec0edeb0311f4c265f6eff5fc2c0db
caiocont <caiobuson@gmail.com>
2021-10-18 22:58:02 -0300

    Cadastro de enventos realizidos

---
+++ pecas.js
@@ -0,0 +1,34 @@
+let peso = 100;
+
+if (peso > 100) 
+{
+    cosnsole.log("A peça possui um peso adequado, pode ser cadastrada!")
+
+} else {
+    console.log ("A peça não pode cadastrada, possui o peso minimo")
+}
+
+let listaPecas  = ["Disco de freio", "Amortercedor", "Virabrequim", "Vela", "Correia", "Bateria", "Radiador", "Carter", "Ignição", "Embreagem"]
+
+let numerodePecas = listaPecas.length;
+
+if (listaPecas.length < 10) {
+    console,log ("A lista possui espaço para mais pecas.")
+ 
+} else {
+    console.log ("Não possui espaço dsiponivel da lista, a capacidade maxima  foi atingida") 
+
+}
+
+let texto = "35"
+let numero =  35
+
+let nomePeca = "Correia"
+
+if( nomePeca.length > 3 ) {
+    console.log("Nome da peça e valido, pode seguir com o cadastro")
+} else {
+    console.log ("Nome de peça invalido, nome deve ter mais 3 caracteres")    
+}
+    
+
